ee81558a946dab9f32a70b15e877e639d57dd0df
dannykil <danniel.kil@gmail.com>
2023-06-08 22:05:53 +0900

    Initial Commit

---
+++ src/components/BookItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+// 구조분할 방법 1
+const BookItem = (props) => {
+  const { id, title, author } = props.book;
+  // 구조분할 방법 2
+  // const BookItem = ({ id, title, author }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Link to={'/book/' + id} className="btn btn-primary" variant="primary">
+          상세보기
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BookItem;
---
+++ src/pages/book/Home.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import BookItem from '../../components/BookItem';
+
+const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  // 함수 실행 시 최초 한 번 실행되는 것 + 상태값이 변경될 때마다 실행
+  useEffect(() => {
+    // 비동기 함수
+    // 왜 then이 2개인가?
+    // 첫번째 인자에는 빈배열을 반환(오래걸릴 수 있으니-promise라고 한다), 그릐고 두번째에 데이터를 전달한다.
+    fetch('http://localhost:8080/book', { method: 'GET' })
+      .then((res) => res.json()) // 응답이 온 데이터를 json object로 변환하겠다
+      .then((res) => {
+        console.log(1, res);
+        setBooks(res);
+      });
+  }, []); // 마지막에 빈배열을 넣어야하며([]) books를 넣는 경우 계속 상태가 변하기 때문에 무한루프를 돌게된다.
+  // CORS 오류가 발생하는데 이것은 외부에서 자바스크립트 요청이 들어오는 것을 막는 것이다.
+
+  // useEffect(() => {
+  //   effect;
+  //   return () => {
+  //     cleanup;
+  //   };
+  // }, [input]);
+
+  return (
+    <div>
+      <h1>책 리스트 보기</h1>
+      {books.map((book) => (
+        <BookItem key={book.id} book={book} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
